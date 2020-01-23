986ff78bd70362b0ea4d05d0f936ed74831d54ae
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-01-23 20:37:15 +0100

    Buggy choropleth
    Altair chart works but not in Streamlit

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import altair as alt
 import pandas as pd
+import geopandas as gpd
 
 
 @st.cache
-def get_data(data_path: str = 'data/african_data.csv', indicators_path: str = '') -> pd.DataFrame:
+def get_data(
+    data_path: str = "data/african_data.csv", indicators_path: str = ""
+) -> pd.DataFrame:
     """Returns dataframe optionally filtered on Indicator Code if indicators_path is given"""
     data = pd.read_csv(data_path)
     if not indicators_path:
         return data
     else:
         indicators = pd.read_csv(indicators_path)
-        return data[data['Indicator Code'].isin(indicators['Indicator Code'])].reset_index(drop=True)
+        return data[
+            data["Indicator Code"].isin(indicators["Indicator Code"])
+        ].reset_index(drop=True)
 
 
 @st.cache
@@ -21,30 +26,42 @@ def wide2long_format(df: pd.DataFrame) -> pd.DataFrame:
     I.e. single years are trasposed as a column and their value as another.
     Like un-pivoting a table on years.
     """
-    identifying_vars = ['Country Code', 'Country Name',
-                        'Indicator Code', 'Indicator Name']
+    identifying_vars = [
+        "Country Code",
+        "Country Name",
+        "Indicator Code",
+        "Indicator Name",
+    ]
     years = [str(x) for x in range(1960, 2020)]
-    return df.melt(id_vars=identifying_vars, value_vars=years, var_name='Year')
+    return df.melt(id_vars=identifying_vars, value_vars=years, var_name="Year")
 
 
-data = get_data(indicators_path='data/indicators/education.csv')
+data = get_data(indicators_path="data/indicators/education.csv")
 
-government_expenditure = wide2long_format(data[data['Indicator Code'] ==
-                                               'SE.XPD.TOTL.GD.ZS']).dropna().reset_index(drop=True)
-
-government_expenditure_2010 = government_expenditure[government_expenditure['Year'] == '2010']
-
-st.markdown('# Education in Africa')
-
-source = alt.topo_feature(
-    'https://raw.githubusercontent.com/deldersveld/topojson/master/continents/africa.json', "continent_Africa_subunits")
-
-chart = alt.Chart(source).mark_geoshape(fill='white', stroke='black')
-chart = chart.encode(color='value:Q')
-chart = chart.transform_lookup(
-    lookup='geounit',
-    from_=alt.LookupData(government_expenditure_2010,
-                         key='Country Name', fields=['value'])
+# Only indicators for education with more than 40 data points in 2010:
+# ['SE.COM.DURS','SE.PRM.DURS','SE.PRM.AGES','SE.PRE.DURS','SE.SEC.DURS','SE.SEC.AGES']
+primary_duration = (
+    wide2long_format(data[data["Indicator Code"] == "SE.PRM.DURS"])
+    .dropna()
+    .reset_index(drop=True)
 )
 
+primary_duration_2010 = primary_duration[primary_duration["Year"] == "2010"]
+
+st.markdown("# Education in Africa")
+
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+africa = world[world.continent == "Africa"]
+africa_geometry = africa[["iso_a3", "geometry"]]
+
+
+source = pd.merge(
+    primary_duration_2010,
+    africa_geometry,
+    left_on="Country Code",
+    right_on="iso_a3",
+    how="left",
+)
+geo_source = gpd.GeoDataFrame(source, geometry="geometry")
+chart = alt.Chart(geo_source).mark_geoshape().encode(color="value:O")
 st.altair_chart(chart)
