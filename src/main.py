f910ed4b78e840748f01b5be361d882da569de52
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-01-24 16:11:17 +0100

    Scatter plot education v women's rights
    refs #5

--- src/main.py
+++ src/main.py
@@ -48,8 +48,6 @@ def plot_choropleth() -> None:
 
     primary_duration_2010 = primary_duration[primary_duration["Year"] == "2010"]
 
-    st.markdown("# Education in Africa")
-
     world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
     africa = world[world.continent == "Africa"]
     africa_geometry = africa[["iso_a3", "geometry"]]
@@ -69,22 +67,58 @@ def plot_choropleth() -> None:
 def plot_scatter():
     ed_data = get_data(indicators_path="data/indicators/education.csv")
     ed_data = wide2long_format(ed_data).dropna()
-
-    top_ed_indicators = ed_data["Indicator Code"].value_counts()[:10].index
-    ed_data = ed_data[ed_data["Indicator Code"].isin(top_ed_indicators)]
-
+    top_ed_indicators = ed_data["Indicator Name"].value_counts()[:10].index
+    ed_data = ed_data[ed_data["Indicator Name"].isin(top_ed_indicators)]
     ed_indicator = st.selectbox(
-        label="Education Indicator", options=ed_data["Indicator Code"].unique()
+        label="Education Indicator", options=ed_data["Indicator Name"].unique()
     )
-    indicator_data = ed_data[ed_data["Indicator Code"] == ed_indicator]
+    ed_indicator_data = ed_data[ed_data["Indicator Name"] == ed_indicator]
+
+    women_data = get_data(indicators_path="data/indicators/women.csv")
+    women_data = wide2long_format(women_data).dropna()
+    top_women_indicators = women_data["Indicator Name"].value_counts()[:10].index
+    women_data = women_data[women_data["Indicator Name"].isin(top_women_indicators)]
+    women_indicator = st.selectbox(
+        label="Women's rights Indicator", options=women_data["Indicator Name"].unique()
+    )
+    women_indicator_data = women_data[women_data["Indicator Name"] == women_indicator]
+
     year = str(st.slider(label="Year of interest", min_value=1970, max_value=2019))
     # year = st.selectbox(
-    #     label="Year of interest", options=indicator_data["Year"].unique()
+    #     label="Year of interest", options=ed_indicator_data["Year"].unique()
     # )
-    final_ed_data = indicator_data[indicator_data["Year"] == year]
-    st.write(final_ed_data)
+    final_ed_data = ed_indicator_data[ed_indicator_data["Year"] == year]
+    final_women_data = women_indicator_data[women_indicator_data["Year"] == year]
+    columns = ["Country Code", "Country Name", "value"]
+    plot_data = pd.merge(
+        final_women_data[columns],
+        final_ed_data[columns],
+        on=["Country Code", "Country Name"],
+        suffixes=("_women", "_education"),
+    )
+
+    if not plot_data.empty:
+        women_is_ordinal = set(final_women_data["value"].unique().tolist()) == {
+            0.0,
+            1.0,
+        }
+        type_women = "ordinal" if women_is_ordinal else "quantitative"
+        chart = (
+            alt.Chart(plot_data)
+            .mark_point()
+            .encode(
+                x=alt.X("value_women", title=women_indicator, type=type_women),
+                y=alt.Y("value_education", title=ed_indicator),
+                tooltip="Country Name",
+            )
+            .properties(height=400, width=600)
+        )
+        st.altair_chart(chart)
+    else:
+        st.markdown("### No data for that year!")
 
 
+st.markdown("# Education in Africa")
 option = st.selectbox("Plot to render", ["scatter", "map"])
 
 if option == "scatter":
