93cf550b9911f31a23fa86191bc7866d2c662b6b
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2020-02-01 11:54:44 +0100

    Add bar chart for map indicator
    Has small bug: a null value is always present

--- src/Education in Africa.py
+++ src/Education in Africa.py
@@ -30,7 +30,7 @@ def wide2long_format(df: pd.DataFrame) -> pd.DataFrame:
         "Country Name",
         "Indicator Code",
         "Indicator Name",
-        "Geo Name"
+        "Geo Name",
     ]
     years = [str(x) for x in range(1960, 2020)]
     return df.melt(id_vars=identifying_vars, value_vars=years, var_name="Year")
@@ -54,83 +54,122 @@ def plot_choropleth() -> None:
     )
 
     # min: 0h, max: 23h, default: 17h
-    year_to_filter = st.slider('Year', 1960, 2019, 2010)
+    year_to_filter = st.slider("Year", 1960, 2019, 2010)
     filtered_data = data[data.Year.str.contains(str(year_to_filter))]
 
-    selection = alt.selection_multi(fields=['properties.geounit'])
-
-    color_scheme = st.sidebar.selectbox("Color scheme", [
-                                        "yellowgreenblue", "greens", 'yellowgreen', 'redpurple', 'goldgreen'], index=4)
-    # We are coloring states with value 0, that's bad
-    color = alt.condition(selection, alt.Color(
-        'value:Q', 
-        scale=alt.Scale(scheme=color_scheme), 
-        legend=alt.Legend(title='Indicator value'), 
-    ), alt.value('lightgray'))
-
-    # The lookup isn't working because name in json and name in data do not match (i.e. Egypt and Egypt, Arab Rep.)
-    # Workaround: change names in data to match json, or get better json with ISO-3 code and match on that with data
-    african_countries = alt.topo_feature(
-        'https://raw.githubusercontent.com/deldersveld/topojson/master/continents/africa.json',
-        'continent_Africa_subunits')
-
-    african_outline = alt.Chart(african_countries).mark_geoshape(
-        stroke='white',
-        strokeWidth=2,
-        fill='lightgrey'
-    ).encode(
-        tooltip=[alt.Tooltip('properties.geounit:O', title='Country name'),
-                 alt.Tooltip('value:Q', title='Indicator value')],
-    ).transform_lookup(
-        lookup='properties.geounit',
-        from_=alt.LookupData(filtered_data, 'Geo Name', ['value'])
-    ).properties(
-        width=600,
-        height=500
+    color_scheme = st.sidebar.selectbox(
+        "Color scheme",
+        ["yellowgreenblue", "greens", "yellowgreen", "redpurple", "goldgreen"],
+        index=4,
     )
 
-    africa_chart = alt.Chart(african_countries).mark_geoshape(
-        stroke='white',
-        strokeWidth=2
-    ).encode(
-        color=color,
-        tooltip=[alt.Tooltip('properties.geounit:O', title='Country name'),
-                 alt.Tooltip('value:Q', title='Indicator value')],
-    ).transform_lookup(
-        lookup='properties.geounit',
-        from_=alt.LookupData(filtered_data, 'Geo Name', ['value'])
-    ).properties(
-        width=600,
-        height=500
-    ).add_selection(
-        selection
+    african_countries = alt.topo_feature(
+        "https://raw.githubusercontent.com/deldersveld/topojson/master/continents/africa.json",
+        "continent_Africa_subunits",
+    )
+
+    map_selection = alt.selection_multi(fields=["properties.geounit"])
+
+    bar_color = alt.condition(
+        map_selection,
+        alt.Color("value:Q", scale=alt.Scale(scheme=color_scheme), legend=None),
+        alt.value("lightgrey"),
+    )
+    bar_chart = (
+        alt.Chart(african_countries)
+        .mark_bar()
+        .encode(
+            x=alt.X("Country Code:O", sort="-y"),
+            y=alt.Y("value:Q", title=ed_indicator),
+            color=bar_color,
+            tooltip=[
+                alt.Tooltip("Country Code:O"),
+                alt.Tooltip("properties.geounit:O", title="Country Name"),
+                alt.Tooltip("value:Q", title=ed_indicator),
+            ],
+        )
+        .transform_lookup(
+            lookup="properties.geounit",
+            from_=alt.LookupData(filtered_data, "Geo Name", ["value", "Country Code"]),
+        )
+        .add_selection(map_selection)
+    )
+
+    color = alt.condition(
+        map_selection,
+        alt.Color(
+            "value:Q",
+            scale=alt.Scale(scheme=color_scheme),
+            legend=alt.Legend(title="Indicator value"),
+        ),
+        alt.value("lightgray"),
+    )
+
+    african_outline = (
+        alt.Chart(african_countries)
+        .mark_geoshape(stroke="white", strokeWidth=2, fill="lightgrey")
+        .encode(
+            tooltip=[
+                alt.Tooltip("properties.geounit:O", title="Country name"),
+                alt.Tooltip("value:Q", title="Indicator value"),
+            ],
+        )
+        .transform_lookup(
+            lookup="properties.geounit",
+            from_=alt.LookupData(filtered_data, "Geo Name", ["value"]),
+        )
+        .properties(width=600, height=500)
+    )
+
+    africa_chart = (
+        alt.Chart(african_countries)
+        .mark_geoshape(stroke="white", strokeWidth=2)
+        .encode(
+            color=color,
+            tooltip=[
+                alt.Tooltip("properties.geounit:O", title="Country name"),
+                alt.Tooltip("value:Q", title="Indicator value"),
+            ],
+        )
+        .transform_lookup(
+            lookup="properties.geounit",
+            from_=alt.LookupData(filtered_data, "Geo Name", ["value"]),
+        )
+        .properties(width=600, height=500)
+        .add_selection(map_selection)
     )
 
     africa = african_outline + africa_chart
 
-    columns = [str(year) for year in range(
-        max(year_to_filter-5, 1960), min(year_to_filter+6, 2019))]
+    columns = [
+        str(year)
+        for year in range(max(year_to_filter - 5, 1960), min(year_to_filter + 6, 2019))
+    ]
 
-    trends = alt.Chart(african_countries).mark_line(point=True).encode(
-        x='Year:O',
-        y=alt.Y('value:Q', title='Indicator value'),
-        color=alt.Color('properties.geounit:N',
-                        legend=alt.Legend(title='Selected Country')),
-        tooltip=[alt.Tooltip('properties.geounit:O', title='Country name'),
-                 alt.Tooltip('value:Q', title='Indicator value')],
-    ).transform_lookup(
-        lookup='properties.geounit',
-        from_=alt.LookupData(olddata, 'Country Name', columns)
-    ).transform_fold(
-        columns, as_=['Year', 'value']
-    ).properties(
-        width=600,
-        height=500
-    ).transform_filter(
-        selection
+    trends = (
+        alt.Chart(african_countries)
+        .mark_line(point=True)
+        .encode(
+            x="Year:O",
+            y=alt.Y("value:Q", title="Indicator value"),
+            color=alt.Color(
+                "properties.geounit:N", legend=alt.Legend(title="Selected Country")
+            ),
+            tooltip=[
+                alt.Tooltip("properties.geounit:O", title="Country name"),
+                alt.Tooltip("value:Q", title="Indicator value"),
+            ],
+        )
+        .transform_lookup(
+            lookup="properties.geounit",
+            from_=alt.LookupData(olddata, "Country Name", columns),
+        )
+        .transform_fold(columns, as_=["Year", "value"])
+        .properties(width=600, height=500)
+        .transform_filter(map_selection)
     )
 
-    st.write(africa & trends)
+    st.write(bar_chart & africa & trends)
 
 
 def plot_scatter():
@@ -145,15 +184,12 @@ def plot_scatter():
 
     women_data = get_data(indicators_path="data/indicators/women.csv")
     women_data = wide2long_format(women_data).dropna()
-    top_women_indicators = women_data["Indicator Name"].value_counts()[
-        :10].index
-    women_data = women_data[women_data["Indicator Name"].isin(
-        top_women_indicators)]
+    top_women_indicators = women_data["Indicator Name"].value_counts()[:10].index
+    women_data = women_data[women_data["Indicator Name"].isin(top_women_indicators)]
     women_indicator = st.selectbox(
         label="Women's rights Indicator", options=women_data["Indicator Name"].unique()
     )
-    women_indicator_data = women_data[women_data["Indicator Name"]
-                                      == women_indicator]
+    women_indicator_data = women_data[women_data["Indicator Name"] == women_indicator]
 
     eco_data = get_data(indicators_path="data/indicators/economics.csv")
     eco_data = wide2long_format(eco_data).dropna()
@@ -183,9 +219,7 @@ def plot_scatter():
     columns = ["Country Code", "Country Name", "value"]
 
     plot_data = pd.merge(
-        plot_data,
-        final_eco_data[columns],
-        on=["Country Code", "Country Name"],
+        plot_data, final_eco_data[columns], on=["Country Code", "Country Name"],
     )
     plot_data.rename(columns={"value": "value_eco"}, inplace=True)
 
@@ -197,45 +231,55 @@ def plot_scatter():
         type_women = "ordinal" if women_is_ordinal else "quantitative"
 
         brush = alt.selection_interval()  # selection of type "interval"
-        color = alt.condition(brush, alt.value('teal'), alt.value('lightgray'))
+        color = alt.condition(brush, alt.value("teal"), alt.value("lightgray"))
         chart_base = (
-            alt.Chart(plot_data)
-            .mark_point(size=80)
-            .properties(height=400, width=600)
+            alt.Chart(plot_data).mark_point(size=80).properties(height=400, width=600)
         )
         chart = chart_base.encode(
-                x=alt.X("value_women", title=women_indicator, type=type_women),
-                y=alt.Y("value_education", title=ed_indicator),
-                tooltip="Country Name",
-                color=color
-            )
+            x=alt.X("value_women", title=women_indicator, type=type_women),
+            y=alt.Y("value_education", title=ed_indicator),
+            tooltip="Country Name",
+            color=color,
+        )
         chart2 = chart_base.encode(
             x=alt.X("value_eco", title=eco_indicator, type=type_women),
             y=alt.Y("value_education", title=ed_indicator),
             tooltip="Country Name",
-            color=color
+            color=color,
         )
 
-        polynomial_fit = chart.transform_regression(
+        polynomial_fit = (
+            chart.transform_regression(
                 "value_women", "value_education", method="poly", order=1
-        ).mark_line(
-        ).encode(color=alt.value('darkorange'))
-        polynomial_fit2 = chart2.transform_regression(
-            "value_eco", "value_education", method="poly", order=1
-        ).mark_line(
-        ).encode(color=alt.value('darkorange'))
+            )
+            .mark_line()
+            .encode(color=alt.value("darkorange"))
+        )
+        polynomial_fit2 = (
+            chart2.transform_regression(
+                "value_eco", "value_education", method="poly", order=1
+            )
+            .mark_line()
+            .encode(color=alt.value("darkorange"))
+        )
 
         b = st.checkbox(label="Show correlation line", value=True, key=None)
         if b:
-            st.altair_chart(alt.vconcat(chart.add_selection(brush) + polynomial_fit) & alt.vconcat(chart2.add_selection(brush) + polynomial_fit2))
+            st.altair_chart(
+                alt.vconcat(chart.add_selection(brush) + polynomial_fit)
+                & alt.vconcat(chart2.add_selection(brush) + polynomial_fit2)
+            )
         else:
-            st.altair_chart(alt.vconcat(chart.add_selection(brush)) & alt.vconcat(chart2.add_selection(brush)))
+            st.altair_chart(
+                alt.vconcat(chart.add_selection(brush))
+                & alt.vconcat(chart2.add_selection(brush))
+            )
     else:
         st.markdown("### No data for that year!")
 
 
 st.markdown("# Education in Africa")
-option = st.sidebar.selectbox("Plot to render", ['scatter', 'map'])
+option = st.sidebar.selectbox("Plot to render", ["scatter", "map"])
 
 if option == "scatter":
     plot_scatter()
